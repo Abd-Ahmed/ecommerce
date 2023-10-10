c7f1f22e12967aa3113bec94cf4a5916031abfd7
Ahmed Abdennadher <abdahmed2001@gmail.com>
2023-10-10 16:41:09 +0100

    2eme_version

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CategorieController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware('api')->group(function () {
+    Route::resource('categories', CategorieController::class);
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  //  return $request->user();
+//});
